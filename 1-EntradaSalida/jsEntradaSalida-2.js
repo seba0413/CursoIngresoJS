ae7a45ec416c63682ee0ecedffa7f1caeb5f596c
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-02-05 20:54:42 -0300

    ejercicio 1 y 2
    Ver el alert, prompt, variables y concatenar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -3,5 +3,19 @@ y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
 	
+	//alert("anda");
+	var nombre;
+	/*
+	alert("nombre");
+	alert(nombre);
+	nombre ="sebastian";
+	alert(nombre);
+	*/
+	nombre = prompt("ingrese su nombre", "natalia natalia");
+	alert("su nombre es: " + nombre);
+
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
+
+//Variables: Porciones de memoria RAM. Tienen que empezar con una letra, no con un numero y puede empezar, por ejemplo, con un guion
 
