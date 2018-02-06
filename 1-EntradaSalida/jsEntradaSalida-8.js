b9c46497029197bbe2254f4e498b98c8e2282995
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-02-05 22:14:32 -0300

    ejercicio 8
    operacion resto

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,13 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var dividendo = document.getElementById('numeroDividendo').value;
+	var divisor = document.getElementById('numeroDivisor').value;
+
+	var enteroDividendo = parseInt(dividendo);
+	var enteroDivisor = parseInt(divisor);
+
+	var resto = enteroDividendo % enteroDivisor;
+
+	alert("El resto es: " + resto);
 }
