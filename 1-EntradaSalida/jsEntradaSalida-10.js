e34dfa44cc3560b26a414f2a89c17c0b81eff7c4
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-02-06 19:13:23 -0300

    ejercicios 9 y 10
    operaciones con porcentajes

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe = document.getElementById('importe').value;
+	var enteroImporte = parseInt(importe);
+
+	var resultado = enteroImporte - (enteroImporte / 4);
+	
+	document.getElementById('resultado').value = resultado;
+		
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,12 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var importe = document.getElementById('sueldo').value;
+	
+	var sueldo = parseInt(importe);
 
+	var resultado = sueldo + (sueldo / 10);	
+
+	document.getElementById('resultado').value = resultado;
 	
 }
