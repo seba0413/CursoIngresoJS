143bd295f715bdb7b77a186adcfb14dc01a80b8c
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-02-17 10:18:57 -0300

    Ejercicio 10
    Ejercicio corregido

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,19 +1,26 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var random = Math.floor(Math.random()*10);
-	if (random == 9 || random == 10) {
+	var random = Math.floor(Math.random()*10 + 1);
+	
+	if (random > 8) 
+	{
 		alert(random + ". EXCELENTE");
 	}
 
-	else if (random >= 4) {
-		alert(random + ". APROBÓ");
+	else
+	{
+		if (random > 3) 
+		{
+			alert(random + ". APROBÓ");
+
+		}
+
+		else
+		{
+			alert(random + ". Vamos, la proxima se puede");
+		}
+
 	}
-
-	else if (random < 4) {
-		alert(random + ". Vamos, la proxima se puede");
-	}
-
-
 
 }//FIN DE LA FUNCIÓN
