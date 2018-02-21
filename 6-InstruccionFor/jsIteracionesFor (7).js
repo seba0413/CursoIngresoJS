2489cd03928579d99ed95ba86275512b502726fc
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-02-20 22:11:18 -0300

    Ejercicio 7
    Conseguir divisores y numeros primos

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,28 @@
 function Mostrar()
 {
 
+	var numero = prompt("Ingrese un numero");
+	numero = parseInt(numero);
+	var contadorDivisores = 0;
 
+	for (anterior = 2; anterior < numero; anterior++) 
+	{
+		if (numero % anterior == 0) 
+		{
+			document.write("<br>" + anterior);
+			contadorDivisores++;
+
+		}
+		if (anterior > numero / 2) 
+		{
+			break;
+		}
+	}
+
+	if (contadorDivisores == 0) 
+	{
+		document.write("Es primo");
+	}
 
 
 }//FIN DE LA FUNCIÓN
